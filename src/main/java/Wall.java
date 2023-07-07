83977ded4946f915493d0b345f299a5453da9267
Maro 43 <mariusz.olbrys@outlook.com>
2023-07-07 17:21:01 +0200

    Fix methods

--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -11,25 +11,29 @@ public class Wall implements Structure {
     @Override
     public Optional<Block> findBlockByColor(String color) {
         return blocks.stream()
-                .filter(block -> block.getColor().equals(color) ||
-                        (block instanceof CompositeBlock && findByColor((CompositeBlock) block, color).isPresent()))
+                .filter(block -> {
+                    String blockColor = block.getColor();
+                    return blockColor != null && blockColor.equals(color) ||
+                            (block instanceof CompositeBlock && findByColor((CompositeBlock) block, color).isPresent());
+                })
                 .findAny();
     }
 
     @Override
     public List<Block> findBlocksByMaterial(String material) {
-        return blocks.stream()
-                .flatMap(block -> {
-                    if (block.getMaterial().equals(material)) {
-                        return Stream.of(block);
-                    } else if (block instanceof CompositeBlock) {
-                        return findByMaterial((CompositeBlock) block, material).stream();
-                    } else {
-                        return Stream.empty();
-                    }
-                })
-                .toList();
-    }
+            return blocks.stream()
+                    .flatMap(block -> {
+                        String blockMaterial = block.getMaterial();
+                        if (blockMaterial != null && blockMaterial.equals(material)) {
+                            return Stream.of(block);
+                        } else if (block instanceof CompositeBlock) {
+                            return findByMaterial((CompositeBlock) block, material).stream();
+                        } else {
+                            return Stream.empty();
+                        }
+                    })
+                    .toList();
+        }
 
     @Override
     public int count () {
@@ -42,15 +46,19 @@ public class Wall implements Structure {
 
     private Optional<Block> findByColor(CompositeBlock compositeBlock, String color) {
         return compositeBlock.getBlocks().stream()
-                .filter(block -> block.getColor().equals(color) ||
-                        (block instanceof CompositeBlock && findByColor((CompositeBlock) block, color).isPresent()))
+                .filter(block -> {
+                    String blockColor = block.getColor();
+                    return blockColor != null && blockColor.equals(color) ||
+                            (block instanceof CompositeBlock && findByColor((CompositeBlock) block, color).isPresent());
+                })
                 .findFirst();
     }
 
     private List<Block> findByMaterial(CompositeBlock compositeBlock, String material) {
         return compositeBlock.getBlocks().stream()
                 .flatMap(block -> {
-                    if (block.getMaterial().equals(material)) {
+                    String blockMaterial = block.getMaterial();
+                    if (blockMaterial != null && blockMaterial.equals(material)) {
                         return Stream.of(block);
                     } else if (block instanceof CompositeBlock) {
                         return findByMaterial((CompositeBlock) block, material).stream();
